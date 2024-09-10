24bb38c42569acbdc98c3c3670165369ab3e96c0
Bruno Duarte <brunosduarte@hotmail.com>
2024-09-10 11:54:24 -0300

    feat: routes implementation

---
+++ src/http/routes/create-goal.ts
@@ -0,0 +1,21 @@
+import { createGoal } from "@/functions/create-goal"
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import z from "zod"
+
+export const createGoalRoute: FastifyPluginAsyncZod = async app => {
+  app.post('/goals', {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      })
+    }
+  }, async (request) => {
+      const { title, desiredWeeklyFrequency } = request.body
+      await createGoal({
+        title,
+        desiredWeeklyFrequency,
+      })
+    }
+  )
+}
