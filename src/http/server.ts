5133c0e761121f539e22ccd73c271888f5a65669
Bruno Duarte <brunosduarte@hotmail.com>
2024-09-10 14:18:20 -0300

    fix: issues on completions

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
-import fastify from "fastify"
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
-import { createGoalRoute } from "./routes/create-goal"
-import { getPendingGoalsRoute } from "./routes/get-pending-goals"
-import { createCompletionRoute } from "./routes/create-completion"
-import { getWeekSummaryRoute } from "./routes/get-week-summary"
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -15,8 +24,10 @@ app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
 app.register(getWeekSummaryRoute)
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
