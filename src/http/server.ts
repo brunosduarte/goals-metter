56bf54234ea6ebac2d6df897836c4bae68cb52ff
Bruno Duarte <brunosduarte@hotmail.com>
2024-09-10 01:35:22 -0300

    feat: create-goal route implementation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
+import { createGoal } from "@/functions/create-goal"
 import fastify from "fastify"
+import z from "zod"
 
 const app = fastify()
+
+app.post('/goals', async (request) => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+  
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency
+  })
+})
 
 app.listen({
   port: 3333,
